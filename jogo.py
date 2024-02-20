5be8fd972339ce70b4dc41d7a197211c314986e0
buenogi <gislayne.bueno@gmail.com>
2024-02-20 11:49:03 -0300

    Pseudo-código

---
+++ jogo.py
@@ -0,0 +1,10 @@
+# 1. Mensagem de boas vindas
+# 2. Definir lista de palavras que podem ser advinhadas
+# 3. Definir o numero de tentativas que o usuário terá
+# 4. Imprimir na tela o numero de letras da palavra
+# 5. Receber a tentativa do usuário
+# 6. Verificar a tentativa do usuário
+# 7. Imprimir, na palavra, onde a tentativa se encaixa
+# 8. Informar caso a tentativa tenha sido falha
+# 9. Atualizar o numero de chances 
+#10. Repetir o processo até que a palavra tenha sido descoberta ou o numero de chances zere
